45b60f940e4d125a6ea7fc43224004f670fb45a7
eugenevu52 <eugenevu52>
2019-09-03 16:33:13 +0700

    .

---
+++ 0509/index.js
@@ -0,0 +1,12 @@
+let fiBtn = document.getElementById('firstButton');
+let seBtn = document.getElementById('secondButton');
+let thiBtn = document.getElementById('thirdButton');
+fiBtn.addEventListener('click',()=>{
+    console.log('1');
+})
+seBtn.addEventListener('click',()=>{
+    console.log('2');
+})
+thiBtn.addEventListener('click',()=>{
+    console.log('3');
+})
